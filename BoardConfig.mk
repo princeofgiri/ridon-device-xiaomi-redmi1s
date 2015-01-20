349a680ecafd18c62824c7a7c79de9a850a71e92
ahmad haris <haris@air>
2015-01-21 05:52:07 +0700

    change TARGET_KERNEL_CONFIG

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 # Kernel
 TARGET_PREBUILT_KERNEL := device/xiaomi/armani/kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/armani
-TARGET_KERNEL_CONFIG := cm_msm8226_perf_defconfig
+TARGET_KERNEL_CONFIG := msm8226-perf_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
